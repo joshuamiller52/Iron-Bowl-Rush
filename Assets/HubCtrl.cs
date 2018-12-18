f61b85ed459397107504e0b9d0a7c8d133948fd8
Joshua Miller <josde011@gmail.com>
2018-12-17 23:04:52 -0600

    Initial commit with ability to make one enemy

---
+++ Assets/HubCtrl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HubCtrl : MonoBehaviour
+{
+
+    public int Health { get; private set; }
+    // Use this for initialization
+    void Start()
+    {
+        Health = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RemoveHealth(int hitPoints)
+    {
+        Health -= hitPoints;
+    }
+}
---
+++ Assets/Master.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Master : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        var enemy = CreateEntity(Tags.Enemy);
+        enemy.transform.position = new Vector3(0, 10, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private GameObject CreateEntity(string entityName)
+    {
+        return Instantiate(Resources.Load<GameObject>("prefabs/" + entityName));
+    }
+}
